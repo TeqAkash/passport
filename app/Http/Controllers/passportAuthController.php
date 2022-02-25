bfb5f9c382703b51413df91eb53e04834d56519c
AkashJain1811 <ajatthework18@gmail.com>
2022-02-25 12:09:29 +0530

    passport

--- app/Http/Controllers/passportAuthController.php
+++ app/Http/Controllers/passportAuthController.php
@@ -5,44 +5,57 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Laravel\Sanctum\NewAccessToken;
+use Validator;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class passportAuthController extends Controller
 {
     public function registerUserExample(Request $request){
-        $this->validate($request,[
+         $validator = Validator::make($request->all(),[
             'name'=>'required',
-            'email'=>'required|email|unique:users',
+            'email'=>'required|email|unique:users,email',
             'password'=>'required|min:8',
         ]);
+          if ($validator->fails()) {
+            return response()->json(['status' => false, 'response' => new \stdClass(), 'message' => $validator->errors()->first(), 'code' => Response::HTTP_UNPROCESSABLE_ENTITY], Response::HTTP_OK);
+        }
 
         $user= User::create([
             'name' =>$request->name,
             'email'=>$request->email,
             'password'=>bcrypt($request->password)
         ]);
-            //dd($user->createToken('PassportExample@Section.io'));
-        //dd($user->createToken($request->email));
+            
         $access_token_example = $user->createToken($request->email)->accessToken;
-        //return the access token we generated in the above step
-        return response()->json(['token'=>$access_token_example],200);
+        return response()->json(['status' => true, 'response' => ['token' => $access_token_example], 'message' => $validator->errors()->first(), 'code' => Response::HTTP_UNPROCESSABLE_ENTITY], Response::HTTP_OK);
+        // return response()->json(['token'=>$access_token_example],200);
     }
 
     /**
      * login user to our application
      */
     public function loginUserExample(Request $request){
+        $validator = Validator::make($request->all(),[
+            'email'=>'required|email',
+            'password'=>'required|min:8',
+        ]);
+          if ($validator->fails()) {
+            return response()->json(['status' => false, 'response' => new \stdClass(), 'message' => $validator->errors()->first(), 'code' => Response::HTTP_UNPROCESSABLE_ENTITY], Response::HTTP_OK);
+        }
+
         $login_credentials=[
             'email'=>$request->email,
             'password'=>$request->password,
         ];
         if(auth()->attempt($login_credentials)){
-            //generate the token for the user
+            
             $user_login_token= auth()->user()->createToken($request->email)->accessToken;
-            //now return this token on success login attempt
+            
             return response()->json(['token' => $user_login_token], 200);
         }
         else{
-            //wrong login credentials, return, user not authorised to our system, return error code 401
+            
             return response()->json(['error' => 'UnAuthorised Access'], 401);
         }
     }
@@ -51,7 +64,7 @@ class passportAuthController extends Controller
      * This method returns authenticated user details
      */
     public function authenticatedUserDetails(){   
-        //returns details
+       
         return auth()->user();
     }   
 }
